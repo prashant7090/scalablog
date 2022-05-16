1dc7affadfd2d7a7336c901eb62b0f875229eb94
Prashant <prashant7090@gmail.com>
2022-05-17 00:04:52 +0530

    added few more functions

--- src/main/scala/MyBlog.scala
+++ src/main/scala/MyBlog.scala
@@ -1,6 +1,6 @@
 object MyBlog {
   def main(args: Array[String]): Unit = {
-    condExample()
+    distinctExample()
   }
 
   //https://imprashantsable.blogspot.com/2022/05/scala-collection-aggregateexample.html
@@ -96,6 +96,76 @@ object MyBlog {
 
     val res4 =  canDivide(0)
     println(s"${res4}") // MatchError: 0 (of class java.lang.Integer)
+  }
+
+  //https://imprashantsable.blogspot.com/2022/05/scala-function-const.html
+  def constExample(): Unit = {
+    val res: Any => Int = Function.const(12)
+    val result = res("passAnything")
+    println(s"Result by passing anything : ${result}" ) // result will be constant for any input as 12.
+    //This can be useful to pass to higher order function
+
+    val someList = List("anything", 8, 1298)
+    val listWithSameElement = someList.map(Function.const(12))
+    println(s"Function.Const pass as higher order function ${listWithSameElement}") //List(12, 12, 12)
+  }
+
+  //https://imprashantsable.blogspot.com/2022/05/scala-function-containsslice.html
+  def containsSliceExample(): Unit = {
+    val input = List(1,2,3,5,6,7)
+    val isSubCollectionExist = input.containsSlice(List(1,2))
+    println(isSubCollectionExist) // true
+    val isSubCollectionExist1 = input.containsSlice(List(1,2,9))
+    println(isSubCollectionExist1) // false
+    val isSubCollectionExist3 = input.containsSlice(List(1,2,3,4,5,6,7))
+    println(isSubCollectionExist3) // false
+  }
+
+  //https://imprashantsable.blogspot.com/2022/05/scala-function-corresponds.html
+  def correspondsExample(): Unit = {
+    def isEven = (input: Int, otherInput: Int) => (input % 2 == 0 && otherInput %2 ==0)
+    val numberList = List(2,4,6,7)
+    val numberList1 = List(22,24,26,28)
+    val output: Boolean = numberList.corresponds(numberList1)(isEven)
+    println(s"Comparing two list using corresponds - example1 : ${output}") //false
+
+    val numberList2 = List(2,4,6)
+    val numberList3 = List(32,44,88)
+    val output1: Boolean = numberList2.corresponds(numberList3)(isEven)
+    println(s"Comparing two list using corresponds - example1 : ${output1}") // true
+  }
+
+  // https://imprashantsable.blogspot.com/2022/05/scala-function-count.html
+  def countExample(): Unit = {
+    def isEven = (input: Int) => (input % 2 == 0)
+    val numberList = List(1,2,3,4,5,6,7,8,9,10)
+    val totalEvenNumbersInList =numberList.count(isEven)
+    println(s"Total Even Numbers in List = ${totalEvenNumbersInList}") //5
+  }
+  
+  //https://imprashantsable.blogspot.com/2022/05/scala-function-diff.html
+  def diffExample(): Unit = {
+    val input1 = List(5,4,3,2,1)
+    val input2 = List(1,2,3)
+    println(s"input1 diff input2 ${input1 diff input2}")//5,4
+    println(s"input2 diff input1 ${input2 diff input1}") //Empty
+  }
+
+  //https://imprashantsable.blogspot.com/2022/05/scala-function-distinct-and-distinctby.html
+  def distinctExample(): Unit = {
+    val input1 = List(5,4,3,33,2,1,1)
+    val distinctInput1 = input1.distinct
+    println(s"distinctInput1 = $distinctInput1") // List(5, 4, 3, 33, 2, 1)
+
+    val multiplyBy2 = ( num: Int ) => num * 2
+    val distinctByInput1 = input1.distinctBy(multiplyBy2)
+    println(s"distinctByInput1 = $distinctByInput1") // List(5, 4, 3, 33, 2, 1)
+
+    val isGreaterThan2 = ( num: Int ) => num > 2
+    val distinctByInput1ForGreaterThan2 = input1.distinctBy(isGreaterThan2)
+    println(s"distinctByInput1ForGreaterThan2 = $distinctByInput1ForGreaterThan2") //List(5, 2)
 
   }
+
+
 }
